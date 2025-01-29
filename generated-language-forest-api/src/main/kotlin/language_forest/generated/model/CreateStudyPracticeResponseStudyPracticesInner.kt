4ed2d477019942dcf571b6aacc3b9d6d32c87c79
kompany11@hanmail.net <kompany11@hanmail.net>
2025-01-29 18:42:51 +0900

    add study practices inner

---
+++ generated-language-forest-api/src/main/kotlin/language_forest/generated/model/CreateStudyPracticeResponseStudyPracticesInner.kt
@@ -0,0 +1,31 @@
+package language_forest.generated.model
+
+import java.util.Objects
+import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.validation.constraints.DecimalMax
+import jakarta.validation.constraints.DecimalMin
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.Size
+import jakarta.validation.Valid
+
+/**
+ * 
+ * @param studyPracticeId 
+ * @param problemNumber 
+ * @param problem 
+ */
+data class CreateStudyPracticeResponseStudyPracticesInner(
+
+    @get:JsonProperty("studyPracticeId") val studyPracticeId: java.util.UUID? = null,
+
+    @get:JsonProperty("problemNumber") val problemNumber: kotlin.Int? = null,
+
+    @get:JsonProperty("problem") val problem: kotlin.String? = null
+    ) {
+
+}
+
