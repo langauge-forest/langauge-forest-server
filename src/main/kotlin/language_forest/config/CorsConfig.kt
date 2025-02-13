c37800ec73b1747ceb8af15a5a8e0714deda175a
11t518s <11t518s20161759@gmail.com>
2025-02-14 01:34:49 +0900

    cors허용 도메인을 추가합니다

--- src/main/kotlin/language_forest/config/CorsConfig.kt
+++ src/main/kotlin/language_forest/config/CorsConfig.kt
@@ -12,7 +12,7 @@ class CorsConfig {
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val config = CorsConfiguration()
-        config.allowedOrigins = listOf("http://localhost:3000", "http://localhost:8080", "http://172.30.1.12:3000") // 허용할 Origin 추가
+        config.allowedOrigins = listOf("http://localhost:3000", "http://localhost:8080", "https://dev.languageforest.in", "https://www.languageforest.in", "https://languageforest.in") // 허용할 Origin 추가
         config.allowedMethods = listOf("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
         config.allowedHeaders = listOf("*") // 모든 헤더 허용
         config.allowCredentials = true // 쿠키 허용
